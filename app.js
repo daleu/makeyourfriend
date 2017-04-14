378ea76d048963e5932f42aab673921e20c431d9
David Aleu Moseguí <david.aleu@est.fib.upc.edu>
2017-04-14 12:28:04 +0200

    bootstrap and jquery added

--- app.js
+++ app.js
@@ -13,6 +13,11 @@ const port = process.env.PORT;
 app.set('view engine', 'pug');
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use('/', express.static(__dirname + '/www')); // redirect root
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+
 app.get('/', (req, res) => {
   db.getNodes()
     .then((nodes) => {
